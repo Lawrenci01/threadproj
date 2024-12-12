549adfce65e19175e6546d04e7549d68c3f501ba
lawrence nodado <rlnodado@gbox.ncf.edu.ph>
2024-12-12 21:34:00 +0800

    Your commit message

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,19 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3007,
-		// Get rid of the CORS error
-		proxy: {
-			"/api": {
-				target: "http://localhost:3006",
-				changeOrigin: true,
-				secure: false,
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    port: 3007,
+    // Get rid of the CORS error
+    proxy: {
+      "/api": {
+        target: "http://localhost:3006",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    outDir: "dist", // Ensures the build output goes to the 'dist' directory
+  },
 });
